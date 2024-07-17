41e673f0d48c68ce0b1ffd7d28a6bad7dde99716
SleepyBee <sleepybee410@gmail.com>
2024-07-18 02:03:18 +0900

    init commit

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "sleepybee-dev-log",
   description: "안드로이드 개발 / 약간의 일상",
   author: "sleepybee",
-  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  siteUrl: "https://sleepybee-dev.github.io",
   links: {
     github: "https://github.com/sleepybee-dev",
   },
